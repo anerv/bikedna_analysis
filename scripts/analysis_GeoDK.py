3b58c4c1741a8fd9ab3927d7d2c01f8dee59b5fc
Ane Rahbek Vierø <vieroe@hotmail.com>
2023-03-22 16:57:13 +0100

    added analysis scripts

---
+++ scripts/analysis_GeoDK.py
@@ -0,0 +1,9 @@
+'''
+TODO: Summary of REFERENCE intrinsic analysis
+- everything in intrinsic summary? - read results, redo tables
+
+TODO: Analysis of spatial autocorrelation of:
+- over/undershoots
+- potential missing edges/component connections
+
+'''
---
+++ scripts/analysis_OSM.py
@@ -0,0 +1,16 @@
+'''
+TODO: Summary of OSM intrinsic analysis
+- everything in intrinsic summary? - read results, redo tables
+
+TODO: Analysis of spatial autocorrelation of:
+- Missing tags (length or count?)
+- over/undershoots
+- potential missing edges/component connections
+'''
+#%%
+import geopandas as gpd
+import h3
+import matplotlib.pyplot as plt
+#%%
+
+# TODO: Write script for reading results - reuse from BikeDNA?
