7aa1ea362270226c08bf8178ca92f47a25989023
Ane Rahbek Vierø <vieroe@hotmail.com>
2023-05-15 16:20:59 +0200

    added pop

--- settings/paths.py
+++ settings/paths.py
@@ -2,7 +2,7 @@ exec(open("../settings/yaml_variables.py").read())
 import pickle
 
 # OSM filepaths
-osm_processed_fp = f"../data/OSM/{study_area}/processed/"
+osm_processed_fp = f"../data/osm/{study_area}/processed/"
 
 osm_graph_fp = osm_processed_fp + "osm_graph.graphml"
 osm_graph_simplified_fp = osm_processed_fp + "osm_simplified_graph.graphml"
@@ -23,19 +23,19 @@ osm_nodes_simplified_joined_fp = (
 
 osm_grid_fp = osm_processed_fp + "grid.parquet"
 osm_intrinsic_grid_fp = (
-    f"../results/OSM/{study_area}/data/grid_results_intrinsic.pickle"
+    f"../results/osm/{study_area}/data/grid_results_intrinsic.pickle"
 )
 
-osm_intrinsic_fp = f"../results/OSM/{study_area}/data/intrinsic_analysis.json"
+osm_intrinsic_fp = f"../results/osm/{study_area}/data/intrinsic_analysis.json"
 
 osm_meta_fp = osm_processed_fp + "osm_meta.json"
 
-osm_results_fp = f"../results/OSM/{study_area}/"
+osm_results_fp = f"../results/osm/{study_area}/"
 
-osm_results_static_maps_fp = f"../results/OSM/{study_area}/maps_static/"
-osm_results_inter_maps_fp = f"../results/OSM/{study_area}/maps_interactive/"
-osm_results_plots_fp = f"../results/OSM/{study_area}/plots/"
-osm_results_data_fp = f"../results/OSM/{study_area}/data/"
+osm_results_static_maps_fp = f"../results/osm/{study_area}/maps_static/"
+osm_results_inter_maps_fp = f"../results/osm/{study_area}/maps_interactive/"
+osm_results_plots_fp = f"../results/osm/{study_area}/plots/"
+osm_results_data_fp = f"../results/osm/{study_area}/data/"
 
 # Reference filepaths
 ref_processed_fp = f"../data/REFERENCE/{study_area}/processed/"
@@ -72,12 +72,16 @@ ref_results_plots_fp = f"../results/REFERENCE/{study_area}/plots/"
 ref_results_data_fp = f"../results/REFERENCE/{study_area}/data/"
 
 # COMPARE DATA FILEPATH
-compare_processed_fp = f"../data/COMPARE/{study_area}/processed/"
+compare_processed_fp = f"../data/compare/{study_area}/processed/"
 
 # COMPARE RESULTS FILEPATHS
 compare_results_fp = f"../results/REFERENCE/{study_area}/"
 
-compare_results_static_maps_fp = f"../results/COMPARE/{study_area}/maps_static/"
-compare_results_inter_maps_fp = f"../results/COMPARE/{study_area}/maps_interactive/"
-compare_results_plots_fp = f"../results/COMPARE/{study_area}/plots/"
-compare_results_data_fp = f"../results/COMPARE/{study_area}/data/"
+compare_results_static_maps_fp = f"../results/compare/{study_area}/maps_static/"
+compare_results_inter_maps_fp = f"../results/compare/{study_area}/maps_interactive/"
+compare_results_plots_fp = f"../results/compare/{study_area}/plots/"
+compare_results_data_fp = f"../results/compare/{study_area}/data/"
+
+# POPULATION RASTER FILEPATHS
+pop_fp_1 = "../data/population/dk/raw/GHS_POP_E2020_GLOBE_R2022A_54009_100_V1_0_R3_C19/GHS_POP_E2020_GLOBE_R2022A_54009_100_V1_0_R3_C19.tif"
+pop_fp_2 = "../data/population/dk/raw/GHS_POP_E2020_GLOBE_R2022A_54009_100_V1_0_R3_C20/GHS_POP_E2020_GLOBE_R2022A_54009_100_V1_0_R3_C20.tif"
