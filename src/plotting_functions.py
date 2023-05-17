1000522d7fa1a003063ad344cb31b61858694a74
Ane Rahbek Vierø <vieroe@hotmail.com>
2023-05-17 14:33:10 +0200

    new func

--- src/plotting_functions.py
+++ src/plotting_functions.py
@@ -1,4 +1,17 @@
+import folium
+import geopandas as gpd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib import cm, colors
+import contextily as cx
+from collections import Counter
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from IPython.display import Image, HTML, display
+
+
+exec(open("../settings/yaml_variables.py").read())
+exec(open("../settings/plotting.py").read())
+# exec(open("../settings/tiledict.py").read())
 
 
 def plot_scatter(
@@ -27,3 +40,59 @@ def plot_scatter(
     )
     plt.xticks([], [])
     plt.yticks([], [])
+
+
+def plot_saved_maps(filepaths, figsize=pdict["fsmap"], alpha=None, plot_res=plot_res):
+    """
+    Helper function for printing saved plots/maps/images (up to two maps plotted side by side)
+
+    Arguments:
+        filepaths(list): list of filepaths of images to be plotted
+        figsize(tuple): figsize
+        alpha(list): list of len(filepaths) with values between 0-1 for setting the image transparency
+
+    Returns:
+        None
+    """
+
+    assert len(filepaths) <= 2, print(
+        "This function cam plot max two images at a time!"
+    )
+
+    if plot_res == "low":
+        filepaths = [f + ".png" for f in filepaths]
+
+        fig = plt.figure(figsize=figsize)
+
+        for i, f in enumerate(filepaths):
+            img = plt.imread(f)
+            ax = fig.add_subplot(1, 2, i + 1)
+
+            if alpha is not None:
+                plt.imshow(img, alpha=alpha[i])
+
+            else:
+                plt.imshow(img)
+
+            ax.set_axis_off()
+
+        fig.subplots_adjust(wspace=0)
+
+    elif plot_res == "high":
+        filepaths = [f + ".svg" for f in filepaths]
+
+        filepaths.reverse()
+
+        html_string = "<div class='row'></div>"
+
+        for i, f in enumerate(filepaths):
+            if alpha is None:
+                img_html = "<img src='" + f + "'style='width:49%'> </img>"
+                html_string = html_string[:17] + img_html + html_string[17:]
+
+            else:
+                if alpha[i] != 0:
+                    img_html = "<img src='" + f + "'style='width:49%'> </img>"
+                    html_string = html_string[:17] + img_html + html_string[17:]
+
+        display(HTML(html_string))
